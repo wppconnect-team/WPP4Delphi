71ff5cb8693ac63fb169777f71bcdee1f2407838
daviladanielc <71415962+daviladanielc@users.noreply.github.com>
2023-04-03 21:08:29 -0300

    - Ajustada a função JS (#52)
    - Removido o tratamento do th_getList que estava duplicado no fonte.
    - Retirado para nao passar mais o options para o JS pois nao tem mais o parâmetro no JS.abr
    
    Co-authored-by: Daniel <daniel.davila@ids.inf.br>

--- Source/View/uTWPPConnect.Console.pas
+++ Source/View/uTWPPConnect.Console.pas
@@ -2873,7 +2873,6 @@ begin
     raise Exception.Create(MSG_ConfigCEF_ExceptConnetServ);
 
   LJS := FrmConsole_JS_VAR_getList;
-  FrmConsole_JS_AlterVar(LJS, '#OPTIONS#',  Trim(Options));
 
   ExecuteJS(LJS, true);
 end;
