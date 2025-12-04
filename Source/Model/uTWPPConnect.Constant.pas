08dbcc8d550f9db63adf91a6660acdc0addf7c1e
MARCELO DOS SANTOS DE OLIVEIRA <marcelo.broz@hotmail.com>
2025-12-04 13:39:31 -0300

    Fix envio mensagem

--- Source/Model/uTWPPConnect.Constant.pas
+++ Source/Model/uTWPPConnect.Constant.pas
@@ -45,7 +45,7 @@ Uses
 Const
   //Uso GLOBAL
                                   //Version updates I=HIGH, II=MEDIUM, III=LOW, IV=VERY LOW
-  TWPPConnectVersion              = '5.0.0.6'; //  25/11/2025
+  TWPPConnectVersion              = '5.0.0.7'; //  04/12/2025
   CardContact                     = '@c.us';
   CardGroup                       = '@g.us';
   CardList                        = '@broadcast';
